6b9d74641c0510366c1f173033a930d50462d0ae
Kimi <lys.kimi@gms.tku.edu.tw>
2022-02-06 16:35:56 +0800

    Add: gitHub repo

--- src/components/GithubBanner.js
+++ src/components/GithubBanner.js
@@ -4,7 +4,7 @@ import GithubCorner from 'react-github-corner';
 const GithubBanner = () => (
   <div className="github-banner">
     <GithubCorner
-      href="https://github.com/pamcy/React-ToDoList-App"
+      href="https://github.com/a3216lucy/react-todo"
       bannerColor="#283044"
       size="77"
       target="_blank"
