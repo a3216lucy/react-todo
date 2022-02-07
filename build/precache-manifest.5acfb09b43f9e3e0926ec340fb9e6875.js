ebaa781c2a414f18f4f1c150cc5f310a44acb207
Kimi <lys.kimi@gms.tku.edu.tw>
2022-02-07 15:02:28 +0800

    Update: config

---
+++ build/precache-manifest.5acfb09b43f9e3e0926ec340fb9e6875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537d9ef528c456108e36dfa4f3f7a218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2be9dc4a548053ea015",
+    "url": "/static/css/main.c1fd3115.chunk.css"
+  },
+  {
+    "revision": "9622b164704e6b15a2f1",
+    "url": "/static/js/2.2bdb2760.chunk.js"
+  },
+  {
+    "revision": "f2be9dc4a548053ea015",
+    "url": "/static/js/main.fb1bdd3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
