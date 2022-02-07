970b38080a162d55518ffcc050c08235cf09acd6
Kimi <lys.kimi@gms.tku.edu.tw>
2022-02-07 21:10:04 +0800

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './layout/index.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
